876c6b10683ae9d1acf43aedd4c4acecd303cad2
sanchay151 <sanchaytuteja@gmail.com>
2025-02-03 19:25:29 +0530

    srs

--- server/index.js
+++ server/index.js
@@ -43,12 +43,7 @@ app.get("/", (req, res) => {
 });
 
 // Handle 404 errors
-app.use((req, res, next) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found",
-  });
-});
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
